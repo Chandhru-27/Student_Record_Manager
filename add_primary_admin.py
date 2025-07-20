54c1332d677cb1500a487f02e91fcf60dd7a61a4
Chandhru Loganathan <chandhru.l2007@gmail.com>
2025-07-20 16:45:32 +0530

    few enhancements in database connection

--- add_primary_admin.py
+++ add_primary_admin.py
@@ -7,20 +7,20 @@ import os
 load_dotenv()
 
 # Get env vars
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_NAME = os.getenv("DB_NAME", "student_db")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Connect to DB
 try:
     connection = mysql.connector.connect(
-        host="localhost",
-        port="3306",
-        user="root",
-        password="123456",
-        database="student_db"
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
     )
     cursor = connection.cursor()
     if connection:
